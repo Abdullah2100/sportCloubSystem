962574a9900373c9c3deafc180850d9fcb939700
Abdullah2100 <ali735501225@gmail.com>
2024-02-24 09:38:26 +0300

    complate data and buiseness layer for coachTraingin

---
+++ SportCllubeBuisness/clsCoachTrainingBuisness.cs
@@ -0,0 +1,177 @@
+﻿using sportDataLayer;
+using System.Data;
+
+namespace SportCllubeBuisness
+{
+    public class clsCoachTrainingBuisness : clsPeopleBuisness
+    {
+        public enum enMode { add, update };
+        public enMode mode { get; set; }
+        public int id { get; set; }
+        public int coachID { get; set; }
+        public int sportID { get; set; }
+        public int trainingDay { get; set; }
+        public string dayilyStartAt { get; set; }
+        public string dayilyEndAt { get; set; }
+        public bool isAvaliable { get; set; }
+        public double fee { get; set; }
+
+        public clsCoachTrainingBuisness()
+        {
+
+            this.mode = enMode.add;
+            this.id = 0;
+            this.coachID = 0;
+            this.sportID = 0;
+            this.trainingDay = 0;
+            this.dayilyStartAt = "";
+            this.dayilyEndAt = "";
+            this.isAvaliable = false;
+            this.fee = 0;
+
+        }
+
+        private clsCoachTrainingBuisness(enMode mode, int id, int coachID, int sportID, int trainingDay, string dayilyStartAt, string dayilyEndAt, bool isAvaliable, double fee)
+        {
+            this.mode = mode;
+            this.id = id;
+            this.coachID = coachID;
+            this.sportID = sportID;
+            this.trainingDay = trainingDay;
+            this.dayilyStartAt = dayilyStartAt;
+            this.dayilyEndAt = dayilyEndAt;
+            this.isAvaliable = isAvaliable;
+            this.fee = fee;
+        }
+
+        public static clsCoachTrainingBuisness findCoachTrainingByID(int id)
+        {
+
+            int coachID = 0;
+            int sportID = 0;
+            bool isAvaliable = false;
+            string dayilyStartAt = "";
+            string dayilyEndAt = "";
+            int trainingDay = 0;
+            double fee = 0;
+
+            if (clsCoachsTraingingData.findCoachsTrainging(id, ref coachID, ref sportID, ref isAvaliable, ref dayilyStartAt, ref dayilyEndAt, ref trainingDay, ref fee))
+            {
+                return new clsCoachTrainingBuisness(enMode.update, id, coachID, sportID, trainingDay, dayilyStartAt, dayilyEndAt, isAvaliable, fee);
+            }
+
+            return null;
+        }
+
+
+        public static clsCoachTrainingBuisness findCoachTrainingByCoachID(int coachID)
+        {
+
+            int id = 0;
+            int sportID = 0;
+            bool isAvaliable = false;
+            string dayilyStartAt = "";
+            string dayilyEndAt = "";
+            int trainingDay = 0;
+            double fee = 0;
+
+            if (clsCoachsTraingingData.findCoachsTrainging(ref id, coachID, ref sportID, ref isAvaliable, ref dayilyStartAt, ref dayilyEndAt, ref trainingDay, ref fee))
+            {
+                return new clsCoachTrainingBuisness(enMode.update, id, coachID, sportID, trainingDay, dayilyStartAt, dayilyEndAt, isAvaliable, fee);
+            }
+
+            return null;
+        }
+
+
+        private bool _add()
+        {
+            this.id = clsCoachsTraingingData.createCoachesTraineing(
+            this.coachID,
+            this.sportID,
+            this.isAvaliable,
+            this.dayilyStartAt,
+            this.dayilyEndAt,
+            this.trainingDay,
+            this.fee
+                );
+            return (this.id != 0);
+        }
+
+        private bool _update()
+        {
+            return clsCoachsTraingingData.updateCoachTraining(
+                this.id,
+                this.coachID,
+                this.sportID,
+                this.isAvaliable,
+                this.dayilyStartAt,
+                this.dayilyEndAt,
+               this.trainingDay,
+            this.fee);
+        }
+
+
+        public virtual bool save()
+        {
+
+
+
+            switch (mode)
+            {
+                case enMode.add:
+                    {
+                        if (_add())
+                        {
+                            return true;
+                        }
+                        return false;
+                    }
+                case enMode.update:
+                    {
+                        if (_update())
+                            return true;
+                        return false;
+                    }
+            }
+            return false;
+        }
+
+
+        public static DataTable getAllCoachsTraingin()
+        {
+            return clsCoachsTraingingData.getAllcoachTraining();
+        }
+
+        public static DataTable getCoachTrainginByID(int id)
+        {
+            return clsCoachsTraingingData.getAllcoachTraining(id);
+        }
+
+        public static bool deleteCoachTrainginByID(int id)
+        {
+            return clsCoachData.deleteCoach(id);
+        }
+
+
+        public static bool isCoachTainginExistByCoahcID(int coachID)
+        {
+            return clsCoachsTraingingData.isCoachTrainingByCoach(coachID);
+        }
+
+        public static bool activateCoachTraining(int id)
+        {
+            return clsCoachsTraingingData.changeCoachTraingingState(id, true);
+        }
+        public static bool deActivateCoachTraining(int id)
+        {
+            return clsCoachsTraingingData.changeCoachTraingingState(id, true);
+        }
+
+        public static bool isCoachTrainingActive(int id)
+        {
+            return clsCoachsTraingingData.isCoachTrainingActive(id);
+        }
+
+    }
+}
--- sportDataLayer/clsCoachsTraingingData.cs
+++ sportDataLayer/clsCoachsTraingingData.cs
@@ -1,16 +1,61 @@
-﻿namespace sportDataLayer
+﻿using sportDataLayer.Util;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace sportDataLayer
 {
-    static class clsCoachsTraingingData
+    public class clsCoachsTraingingData
     {
-        static bool findCoachsTraingingByID(
-            int id, ref int sportID, ref bool isAvaliable, ref string dayilyStartAt
-, ref string dayilyEndAt,
-ref int trainingDay, ref double fee
-            )
+        public static bool findCoachsTrainging(
+             int id,
+             ref int coacheID,
+             ref int sportID,
+             ref bool isAvaliable,
+             ref string dayilyStartAt,
+             ref string dayilyEndAt,
+             ref int trainingDay,
+             ref double fee
+             )
 
         {
             bool isFound = false;
+            string query = @"select * from CoachesTrainging where CoachsTraingingID = @id";
 
+            try
+            {
+
+                using (SqlConnection con = new SqlConnection(clsConnection.connectionUrl))
+                {
+                    con.Open();
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        cmd.Parameters.AddWithValue("@id", id);
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                isFound = true;
+                                sportID = (int)reader["sportID"];
+                                coacheID = (int)reader["coachID"];
+                                isAvaliable = (bool)reader["isAvaliable"];
+                                dayilyStartAt = (string)reader["dayilyStartAt"];
+                                dayilyEndAt = (string)reader["dayilyEndAt"];
+                                trainingDay = (int)reader["trainingDay"];
+                                fee = (double)reader["fee"];
+
+                            }
+                        }
+
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                clsAppEventHandler.createNewEventLog(ex.Message);
+                Console.WriteLine("errror is : " + ex.Message);
+            }
 
 
 
@@ -19,6 +64,372 @@ ref int trainingDay, ref double fee
 
         }
 
+
+        public static bool findCoachsTrainging(
+             ref int id,
+              int coacheID,
+             ref int sportID,
+             ref bool isAvaliable,
+             ref string dayilyStartAt,
+             ref string dayilyEndAt,
+             ref int trainingDay,
+             ref double fee
+             )
+
+        {
+            bool isFound = false;
+            string query = @"select * from CoachesTrainging where coacheID = @icoacheIDd";
+
+            try
+            {
+
+                using (SqlConnection con = new SqlConnection(clsConnection.connectionUrl))
+                {
+                    con.Open();
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        cmd.Parameters.AddWithValue("@coacheID", coacheID);
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                isFound = true;
+                                id = (int)reader["CoachsTraingingID"];
+                                sportID = (int)reader["sportID"];
+                                isAvaliable = (bool)reader["isAvaliable"];
+                                dayilyStartAt = (string)reader["dayilyStartAt"];
+                                dayilyEndAt = (string)reader["dayilyEndAt"];
+                                trainingDay = (int)reader["trainingDay"];
+                                fee = (double)reader["fee"];
+
+                            }
+                        }
+
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                clsAppEventHandler.createNewEventLog(ex.Message);
+                Console.WriteLine("errror is : " + ex.Message);
+            }
+
+
+
+
+            return isFound;
+
+        }
+
+
+        public static int createCoachesTraineing(
+
+             int coacheID,
+             int sportID,
+             bool isAvaliable,
+             string dayilyStartAt,
+             string dayilyEndAt,
+             int trainingDay,
+             double fee
+            )
+        {
+            int id = 0;
+            string query = @"
+                            insert into CoachsTrainging
+                            (coachID,sportID,isAvaliable,dayilyStartAt,dayilyEndAt,trainingDay,fee)
+                            values(@coachID,@sportID,@isAvaliable,@dayilyStartAt,@dayilyEndAt,@trainingDay,@fee) ;
+                            select SCOPE_IDENTITY()";
+
+            try
+            {
+
+                using (SqlConnection con = new SqlConnection(clsConnection.connectionUrl))
+                {
+                    con.Open();
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        cmd.Parameters.AddWithValue("@coacheID", coacheID);
+                        object result = cmd.ExecuteScalar();
+                        if (result != null && int.TryParse(result.ToString(), out int resultID))
+                        {
+                            id = resultID;
+                        }
+
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                clsAppEventHandler.createNewEventLog(ex.Message);
+                Console.WriteLine("errror is : " + ex.Message);
+            }
+
+            return id;
+        }
+
+        public static bool updateCoachTraining(
+                 int id,
+                 int coachID,
+                 int sportID,
+                 bool isAvaliable,
+                 string dayilyStartAt,
+                 string dayilyEndAt,
+                 int trainingDay,
+                 double fee
+                )
+        {
+            bool isUpdate = false;
+            string query = @"
+                            update CoachsTrainging set
+                            coachID = @coachID
+                           ,sportID=@sportID
+                          ,isAvaliable =@isAvaliable,
+                        ,dayilyStartAt=@dayilyStartAt,
+                        dayilyEndAt = @dayilyEndAt,
+                        trainingDay = @trainingDay,
+                       fee = @fee where CoachsTraingingID = @id";
+            try
+            {
+
+                using (SqlConnection con = new SqlConnection(clsConnection.connectionUrl))
+                {
+                    con.Open();
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        cmd.Parameters.AddWithValue("@id", id);
+                        cmd.Parameters.AddWithValue("@coachID", coachID);
+                        cmd.Parameters.AddWithValue("@sportID", sportID);
+                        cmd.Parameters.AddWithValue("@isAvaliable", isAvaliable);
+                        cmd.Parameters.AddWithValue("@dayilyStartAt", dayilyStartAt);
+                        cmd.Parameters.AddWithValue("@dayilyEndAt", dayilyEndAt);
+                        cmd.Parameters.AddWithValue("@trainingDay", trainingDay);
+                        cmd.Parameters.AddWithValue("@fee", fee);
+                        int result = cmd.ExecuteNonQuery();
+                        if (result > 0)
+                        {
+                            isUpdate = true;
+                        }
+
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                clsAppEventHandler.createNewEventLog(ex.Message);
+                Console.WriteLine("errror is : " + ex.Message);
+            }
+
+            return isUpdate;
+        }
+
+        public static bool changeCoachTraingingState(
+             int id,
+             bool isAvaliable
+            )
+        {
+            bool isUpdate = false;
+            string query = @"update CoachsTrainging set isAvaliable =@isAvaliable where CoachsTraingingID = @id";
+            try
+            {
+
+                using (SqlConnection con = new SqlConnection(clsConnection.connectionUrl))
+                {
+                    con.Open();
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        cmd.Parameters.AddWithValue("@id", id);
+                        cmd.Parameters.AddWithValue("@isAvaliable", isAvaliable);
+                        int result = cmd.ExecuteNonQuery();
+                        if (result > 0)
+                        {
+                            isUpdate = true;
+                        }
+
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                clsAppEventHandler.createNewEventLog(ex.Message);
+                Console.WriteLine("errror is : " + ex.Message);
+            }
+
+            return isUpdate;
+        }
+
+
+
+        public static bool deleteCoachTraining(
+                int id
+                             )
+        {
+            bool isDelete = false;
+            string query = @"delete from CoachsTrainging where CoachsTraingingID = @id";
+            try
+            {
+
+                using (SqlConnection con = new SqlConnection(clsConnection.connectionUrl))
+                {
+                    con.Open();
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        cmd.Parameters.AddWithValue("@id", id);
+                        int result = cmd.ExecuteNonQuery();
+                        if (result > 0)
+                        {
+                            isDelete = true;
+                        }
+
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                clsAppEventHandler.createNewEventLog(ex.Message);
+                Console.WriteLine("errror is : " + ex.Message);
+            }
+
+            return isDelete;
+        }
+
+
+        public static DataTable getAllcoachTraining()
+        {
+            bool isDelete = false;
+            DataTable dtCaochesTraingin = new DataTable();
+            try
+            {
+
+                using (SqlConnection con = new SqlConnection(clsConnection.connectionUrl))
+                {
+                    con.Open();
+                    string query = @"select * from CoachsTrainging";
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+
+                        dtCaochesTraingin.Load(cmd.ExecuteReader());
+
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                clsAppEventHandler.createNewEventLog(ex.Message);
+                Console.WriteLine("errror is : " + ex.Message);
+            }
+
+            return dtCaochesTraingin;
+        }
+
+        public static DataTable getAllcoachTraining(int id)
+        {
+            DataTable dtCaochesTraingin = new DataTable();
+            try
+            {
+
+                using (SqlConnection con = new SqlConnection(clsConnection.connectionUrl))
+                {
+                    con.Open();
+                    string query = @"select * from CoachsTrainging where CoachsTraingingID = @id";
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        cmd.Parameters.Add("@id", id);
+
+                        dtCaochesTraingin.Load(cmd.ExecuteReader());
+
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                clsAppEventHandler.createNewEventLog(ex.Message);
+                Console.WriteLine("errror is : " + ex.Message);
+            }
+
+            return dtCaochesTraingin;
+        }
+
+
+
+        public static bool isCoachTrainingByCoach(int coachID)
+        {
+            bool isFound = false;
+            string query = @"select  found=1 CoachsTrainging where coachID = @coachID ";
+            try
+            {
+
+                using (SqlConnection con = new SqlConnection(clsConnection.connectionUrl))
+                {
+                    con.Open();
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        cmd.Parameters.AddWithValue("@coachID", coachID);
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+
+                            if (reader.HasRows)
+                            {
+                                isFound = true;
+                            }
+
+                        }
+
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                clsAppEventHandler.createNewEventLog(ex.Message);
+                Console.WriteLine("errror is : " + ex.Message);
+            }
+
+            return isFound;
+        }
+
+
+        public static bool isCoachTrainingActive(int id)
+        {
+            bool isActive = false;
+            string query = @"select found=1 CoachsTrainging where CoachsTraingingID = @id and isAvaliable =1 ";
+            try
+            {
+
+                using (SqlConnection con = new SqlConnection(clsConnection.connectionUrl))
+                {
+                    con.Open();
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        cmd.Parameters.AddWithValue("@id", id);
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+
+                            if (reader.HasRows)
+                            {
+                                isActive = true;
+                            }
+
+                        }
+
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                clsAppEventHandler.createNewEventLog(ex.Message);
+                Console.WriteLine("errror is : " + ex.Message);
+            }
+
+            return isFound;
+        }
 
 
     }
